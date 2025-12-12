50bd8ac7a01d9c8658b389cb0ea014ed00d540e5
Jasmine Seerha (300184837) <jasmine.seerha@student.ufv.ca>
2025-12-11 17:07:00 -0800

    Implemented spark particle effects for breakable objects (without auto-emit)

--- breaker_area.gd
+++ breaker_area.gd
@@ -1,9 +1,27 @@
 extends Area3D
 
+@export var ParticleController: Node3D
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
-	# Only break things that are marked as breakable
-	if body.is_in_group("breakable") and body.has_method("break_object"):
-		body.break_object()
+	if body == null:
+		return
+
+	if !body.is_inside_tree():
+		return
+
+	if !body.is_in_group("breakable"):
+		return
+
+	if !body.has_method("break_object"):
+		return
+	
+	var origin: Vector3 = body.global_transform.origin
+	
+	#trigger particles
+	if ParticleController and ParticleController.has_method("trigger_at"):
+		ParticleController.trigger_at(origin)
+
+	body.break_object(origin)
